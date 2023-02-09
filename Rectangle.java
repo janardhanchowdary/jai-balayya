1504a31953b238576dcd0610cd9e9b52b5ca3f78
Nithin257 <nithinnithinkn@gmail.com>
2023-02-09 18:20:20 +0530

    jai ballayya

---
+++ Rectangle.java
@@ -0,0 +1,16 @@
+class Rectangle
+{
+static void area ( )
+{
+int w= 20;
+int h= 10;
+int result= w*h;
+System.out.println(result);
+}
+public static void main (String[] args)
+{
+System.out.println("------------------Main Starts---------------------");
+area();
+System.out.println("-----------------Main Ends-----------------------");
+}
+}
